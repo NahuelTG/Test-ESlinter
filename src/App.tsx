3d6755258d895ed9f1ef25ce75e701217829c708
Nahuel <nahueltorrezg@gmail.com>
2024-10-02 18:11:19 -0400

    test: save

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,18 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-                <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
+        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-              <p>
+        <p>
           Edit <code>src/App.tsx</code> and save to test HMR
-              </p>
+        </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
